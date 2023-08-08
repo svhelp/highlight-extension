af9bfdcd4993d7f8dcbdeebabc4ed4dc898638d3
Aleksandr Kravchenko <svhelpp@gmail.com>
2023-08-08 21:42:29 +0200

    - reworked highlighting logic; - implemented reset func; - implemented config change refresh;

--- src/content-script/app.ts
+++ src/content-script/app.ts
@@ -1,50 +1,67 @@
-import { Config } from "./domain/Config"
+import { Config } from "../domain/Config"
+import { storageKey } from "../constants/storageKey"
 
-const storageKey = 'highlight-extension-user-data'
+const blacklistClass = "highlight-extension-blacklist-item"
 
 const processPage = async () => {
-    // chrome.storage.sync.set({[storageKey]: configs})
-
     const storageData = await chrome.storage.sync.get(storageKey)
     const configs: Config[] = storageData[storageKey] ?? []
-
-    console.log(configs)
-
     const config = configs.find(c => new RegExp(c.url, "i").test(window.location.href))
     
-    if (!!config) {
-        console.log("config found")
-        const elementBuckets = config.listLocators.map(locator => document.querySelectorAll(locator) as unknown as HTMLCollectionOf<HTMLElement>)
-        
-        for (const elements of elementBuckets) {
-            for (const element of elements) {
-                console.log("processing element")
-        
-                let content = element.innerHTML
-        
-                if (config.blackList.some(blItem => new RegExp(blItem, "i").test(content))) {
-                    console.log("black-listed item found")
-        
-                    element.style.opacity = "0.3"
+    if (!config) {
+        return
+    }
+
+    console.log("config found")
+    const elementBuckets = config.listLocators.map(locator => document.querySelectorAll(locator) as unknown as HTMLCollectionOf<HTMLElement>)
+    
+    for (const elements of elementBuckets) {
+        for (const element of elements) {
+            console.log("processing element")
+    
+            let content = element.innerHTML
+    
+            if (config.blackList.some(blItem => new RegExp(blItem, "i").test(content))) {
+                console.log("black-listed item found")
+    
+                element.classList.add(blacklistClass)
+                continue;
+            }
+    
+            for (const wlItem of config.whiteList) {
+                const wlItemPattern = new RegExp(`(${wlItem})`, "i")
+
+                if (!wlItemPattern.test(content)) {
                     continue;
                 }
-        
-                for (const wlItem of config.whiteList) {
-                    const wlItemPattern = new RegExp(wlItem, "i")
-
-                    if (!wlItemPattern.test(content)) {
-                        continue;
-                    }
-        
-                    console.log("white-listed item found")
-        
-                    content = content.replace(wlItemPattern, '<b style="color: red;">$1</b>')
-                }
-        
-                element.innerHTML = content
+    
+                console.log("white-listed item found")
+    
+                content = content.replace(wlItemPattern, '<span class="highlight-extension-whitelist-item">$1</span>')
             }
+    
+            element.innerHTML = content
         }
     }
 }
+
+const reset = () => {
+    const blacklistedItems = document.querySelectorAll(`.${blacklistClass}`)
+
+    for (const item of blacklistedItems) {
+        item.classList.remove(blacklistClass)
+    }
+
+    const content = document.body.innerHTML
+    document.body.innerHTML = content.replace(/\<span class=\"highlight-extension-whitelist-item\"\>(.+?)\<\/span\>/g, '$1')
+}
+
+chrome.storage.onChanged.addListener(
+    () => {
+        console.log("storage changed")
+        reset()
+        processPage()
+    }
+)
 
 processPage()
